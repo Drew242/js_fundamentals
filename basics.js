600c1e0e6b5361fedb235f483cbdb76f042d83c9
Drew Conly <atconly@gmail.com>
2016-04-30 18:42:29 -0600

    create fizzbuzz with js

--- basics.js
+++ basics.js
@@ -1,12 +1,28 @@
-var readline = require('readline');
+// var readline = require('readline');
+//
+// var rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+//
+// rl.question("What's your name? ", function(answer) {
+//   console.log("Welcome to the show", answer);
+//
+//   rl.close();
+// });
 
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// console.log('printing the triangle')
+//
+// for (var hashtag = '#'; hashtag.length <= 10; hashtag = hashtag + '#')
+//   console.log(hashtag);
 
-rl.question("What's your name? ", function(answer) {
-  console.log("Welcome to the show", answer);
-
-  rl.close();
-});
+for (var i=1; i <= 100; i++){
+  if (i % 15 == 0)
+    console.log("Fizzbuzz");
+  else if (i % 3 == 0)
+    console.log("Fizz");
+  else if (i % 5 == 0)
+    console.log("Buzz");
+  else
+    console.log(i); 
+}
